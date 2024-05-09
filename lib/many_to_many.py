98a55d571fdb54e5c1e1659b7e432db773b236ca
Andrej <andrejtodoroski@yahoo.com>
2024-05-09 10:48:12 -0400

    no errors

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,90 +1,87 @@
 class Author:
-    
-    all = []
-
-    def __init__(self, name):
+    def __init__(self, name:str):
         self.name = name
-        Author.all.append(self)
 
     def contracts(self):
         return [contract for contract in Contract.all if contract.author == self]
-
+    
     def books(self):
-        return [contract.book for contract in self.contracts()]
-
+        return [contract.book for contract in Contract.all if contract.author == self]
+    
     def sign_contract(self, book, date, royalties):
         return Contract(self, book, date, royalties)
-
+    
     def total_royalties(self):
-        return sum([contract.royalties for contract in self.contracts()])
-
-
+        return sum([contract.royalties for contract in Contract.all if contract.author == self])
+    
 class Book:
-
-    all = []
-
-    def __init__(self, title):
+    def __init__(self, title:str):
         self.title = title
-        Book.all.append(self)
 
     def contracts(self):
         return [contract for contract in Contract.all if contract.book == self]
-
+    
     def authors(self):
-        return [contract.author for contract in self.contracts()]
-
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 class Contract:
 
     all = []
 
-    def __init__(self, author, book, date, royalites):
+    def __init__(self, author:Author, book:Book, date:str, royalties:int):
         self.author = author
         self.book = book
         self.date = date
-        self.royalties = royalites
+        self.royalties = royalties
         Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
 
     @property
     def author(self):
         return self._author
-
+    
     @author.setter
     def author(self, value):
-        if not isinstance(value, Author):
-            raise Exception
-        self._author = value
-
+        if type(value) == Author:
+            self._author = value
+        else:
+            raise TypeError("Must be of type Author")
+    
     @property
     def book(self):
         return self._book
-
+    
     @book.setter
     def book(self, value):
-        if not isinstance(value, Book):
-            raise Exception
-        self._book = value
-
+        if type(value) == Book:
+            self._book = value
+        else:
+            raise TypeError("Must be of type Book")
+        
     @property
     def date(self):
         return self._date
-
+    
     @date.setter
     def date(self, value):
-        if not isinstance(value, str):
-            raise Exception
-        self._date = value
-
+        if type(value) == str:
+            self._date = value
+        else:
+            raise TypeError("Must be of type string")
+        
     @property
     def royalties(self):
         return self._royalties
-
+    
     @royalties.setter
     def royalties(self, value):
-        if not isinstance(value, int):
-            raise Exception
-        self._royalties = value
-
-    @classmethod
-    def contracts_by_date(cls):
-        return sorted(cls.all, key=lambda c: c.date)
+        if type(value) == int:
+            self._royalties = value
+        else:
+            raise TypeError("Must be of type integer")
+        
+    
+    
